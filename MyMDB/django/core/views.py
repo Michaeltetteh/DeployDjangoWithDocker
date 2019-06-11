23fb7959a2c0a1046c9cb99938c6489322732652
mike Tetteh <miketetteh751@outlook.com>
2019-06-11 20:27:42 +0000

    bug fix

--- MyMDB/django/core/views.py
+++ MyMDB/django/core/views.py
@@ -33,23 +33,16 @@ class MovieDetail(DetailView):
         if self.request.user.is_authenticated:
             vote = Vote.objects.get_vote_or_unsaved_blank_vote(
                 movie = self.object,
-                user = self.request.user
-            )
+                user = self.request.user)
             if vote.id:
-                vote_form_url = reverse('core:UpdateVote',
-                                        kwargs={
-                                            'movie_id':vote.movie.id,
-                                            'pk': vote.id
-                                        })
+                vote_form_url = reverse('core:UpdateVote',kwargs={'movie_id':vote.movie.id,'pk': vote.id})
             else:
-                vote_form_url = (reverse('core:CreateVote',kwargs={
-                                        'movie_id': self.object.id
-                                    }))
+                vote_form_url = (reverse('core:CreateVote',kwargs={'movie_id': self.object.id}))
 
-        vote_form = VoteForm(instance=vote)
-        ctx['vote_form'] = vote_form
-        ctx['vote_form_url'] = vote_form_url
-        return ctx
+            vote_form = VoteForm(instance=vote)
+            ctx['vote_form'] = vote_form
+            ctx['vote_form_url'] = vote_form_url
+            return ctx
 class PersonDetail(DetailView):
     queryset = Person.object.all_with_prefetch_movies()
 
